4e6aaef0180fb9d0ec95f5f33e590bfaa3b82fe8
rzknh <144760534+rzknh@users.noreply.github.com>
2024-12-11 14:19:31 +0700

    Add files via upload

---
+++ data employee.sql
@@ -0,0 +1,8 @@
+INSERT INTO Employees (FirstName, LastName, Salary)
+VALUES 
+    ('John', 'Doe', 5000.00),
+    ('Jane', 'Smith', 5500.00),
+    ('Mark', 'Johnson', 6000.00),
+    ('Emily', 'Davis', 4500.00);
+
+	
